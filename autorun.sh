87e40ef21ace411cc026c73e74fec339bdda1122
Daokun Hu <daokunhu@gmail.com>
2022-07-21 20:40:26 +0800

    Update lib path

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,7 @@
-# export LD_LIBRARY_PATH=/usr/local/lib64/vmem_debug:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/aim/hdk/Halo/third/pmdk/src/nondebug:$LD_LIBRARY_PATH
+#Lib path of libvmem
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+# export LD_LIBRARY_PATH=/home/aim/hdk/Halo/third/pmdk/src/nondebug:$LD_LIBRARY_PATH
 for w in ycsba ycsbb ycsbc ycsbd ycsbe ycsbf ycsbg ycsbh ycsbj ycsbak ycsbal ycsbam PiBench1 PiBench2 PiBench3 PiBench4 PiBench5 PiBench6 PiBench7 PiBench8
 do
     for t in 1 4 8 16 24 32
